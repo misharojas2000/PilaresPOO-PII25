d5477416093fca7b2fa57d175b9dde0319d50c73
Fernanda Mishell Monzón Rojas <mishellrojas2016@gmail.com>
2025-07-26 16:07:20 -0600

    Ejemplo en clase

--- PilaresPOO/Program.cs
+++ PilaresPOO/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using PilaresPOO.Clases;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CuentaBancaria cuentaNueva = new CuentaBancaria("jsandoval", "12345678", 300);
+
+        cuentaNueva.Depositar(100);
+
+        Console.WriteLine("Saldo actual: " + cuentaNueva.ObtenerSaldo("jsandoval", "12345678"));
+
+        cuentaNueva.Retirar(100, "jsandoval", "12345678");
+
+        Console.WriteLine("Saldo actual: " + cuentaNueva.ObtenerSaldo("jsandoval", "12345678"));
+
+        cuentaNueva.Retirar(1000, "jsandoval", "12345678");
+
+        Console.WriteLine("Saldo actual: " + cuentaNueva.ObtenerSaldo("jsandoval", "12345678"));
+    }
+}
